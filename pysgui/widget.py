0036fac6cd0ee525f7de38a92e7506677c95e905
ThibClnt <thibaut.colnot@efrei.net>
2025-09-17 23:45:37 +0200

    #2 Create Widget

--- pysgui/widget.py
+++ pysgui/widget.py
@@ -1,10 +1,28 @@
+from __future__ import annotations
+
 import pygame as pg
 
+from .styling import StylableMixin
 
-class Widget:
 
-    def draw(self, surface):
+class Widget(StylableMixin):
+
+    def __init__(self, style_name: str = "Widget"):
+        super().__init__(style_name)
+        self._on_style_change = self._rebuild
+        self._rect: pg.Rect = pg.Rect(0, 0, 0, 0)
+
+    def draw(self, surface: pg.Surface, parent_pos: tuple[int, int] = (0, 0)):
         pass
 
-    def handle_event(self, event: pg.Event) -> bool:
+    def handle_event(self, event: pg.Event, parent_pos: tuple[int, int] = (0, 0)) -> bool:
         return False
+
+    @property
+    def rect(self):
+        return self._rect
+
+    def _rebuild(self):
+        pass
+
+
